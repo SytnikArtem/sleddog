68632acfe151777aa972f78f61e5ba99fb510bfe
SytnikArtem <artemasitnik@gmail.com>
2018-04-15 18:03:03 +0300

    15.04
    18:02

--- gulpfile.js
+++ gulpfile.js
@@ -4,11 +4,17 @@ var gulp = require('gulp');
     concat = require('gulp-concat');
     uglify = require('gulp-uglifyjs');
     cssnano = require('gulp-cssnano');
-    rename = require('gulp-rename')
+    rename = require('gulp-rename');
+    del = require('del');
+    autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('sass',function(){
   return gulp.src('app/sass/**/*.sass')
     .pipe(sass())
+    .pipe(autoprefixer({
+      browsers: ['last 2 version'],
+      cascade: true
+    }))
     .pipe(gulp.dest('app/css'))
     .pipe(browserSync.reload({stream: true}))
 });
@@ -43,4 +49,26 @@ gulp.task('watch', ['browser-sync', 'libs-css', 'jquery'], function() {
     gulp.watch('app/*.html', browserSync.reload);
     gulp.watch('app/js/**/*.js', browserSync.reload);
 });
+
+gulp.task('build', ['sass', 'jquery'], function(){
+  var buildCss = gulp.src([
+    'app/css/main.css',
+    'app/css/libs.min.css'
+  ])
+  .pipe(gulp.dest('dist/css'))
+
+  var buildFonts = gulp.src('app/fonts/**/*')
+  .pipe(gulp.dest('dist/fonts'))
+
+  var buildJs = gulp.src('app/js/**/*')
+  .pipe(gulp.dest('dist/js'))
+
+  var buildHtml = gulp.src('app/*.html')
+  .pipe(gulp.dest('dist'));
+});
+
+gulp.task('clear', function () {
+    return cache.clearAll();
+})
+
 gulp.task('default', ['watch']);
